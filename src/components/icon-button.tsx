246977a21a6f699bfe3f79eccd8475f460e8cfe8
rueljessica <jessruel00@gmail.com>
2025-02-18 18:31:16 -0300

    Adicionando composition pattern, extensão biome e criando página inicial

--- src/components/icon-button.tsx
+++ src/components/icon-button.tsx
@@ -1,12 +1,12 @@
-import { ComponentProps } from "react";
+import type { ComponentProps } from "react";
 
-interface IconButtonProps extends ComponentProps<'button'> {}
+interface IconButtonProps extends ComponentProps<"button"> {}
 
 export function IconButton(props: IconButtonProps) {
-    return (
-        <button 
-            className="p-1.5 bg-gray-500 text-blue rounded-md cursor-pointer transition-colors duration-300 hover:bg-blue hover:text-gray-900"
-            {...props}
-        />
-    )
+	return (
+		<button
+			className="p-1.5 bg-gray-500 text-blue rounded-md cursor-pointer transition-colors duration-300 hover:bg-blue hover:text-gray-900"
+			{...props}
+		/>
+	);
 }
